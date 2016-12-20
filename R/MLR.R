62962247ec3595d48b05e2ff6a063b17a3032764
Adéla Drabinová <adela.drabinova@gmail.com>
2016-12-20 11:03:58 +0100

    Version 1.0.0.
    Viz NEWS file

---
+++ R/MLR.R
@@ -0,0 +1,138 @@
+#' DDF likelihood ratio statistics based on Multinomial Log-linear Regression model.
+#'
+#' @aliases MLR
+#'
+#' @description Calculates DDF likelihood ratio statistics based on multinomial log-linear model.
+#'
+#' @param Data character: the unscored data matrix only.
+#' @param group numeric or character: the binary vector of group membership
+#' @param key character: the answer key.
+#' @param type character: type of DIF to be tested (either "both" (default), "udif", or "nudif").
+#' See \strong{Details}.
+#' @param p.adjust.method character: method for multiple comparison correction.
+#' See \strong{Details}.
+#' @param alpha numeric: significance level (default is 0.05).
+#'
+#' @usage MLR(Data, group, key, type = "both", p.adjust.method = "none", alpha = 0.05)
+#' @details
+#' Calculates DDF likelihood ratio statistics based on multinomial log-linear model.
+#'
+#' The \code{Data} is a matrix whose rows represents examinee unscored answers and
+#' columns correspond to the items. The \code{group} must be a vector of the same
+#' length as \code{nrow(data)}. The \code{key} must be a vector of correct answers
+#' corresponding to columns of \code{Data}.
+#'
+#' The \code{type} corresponds to type of DIF to be tested. Possible values are \code{"both"}
+#' to detect any DIF (uniform and/or non-uniform), \code{"udif"} to detect only uniform DIF or
+#' \code{"nudif"} to detect only non-uniform DIF.
+#'
+#' The \code{p.adjust.method} is a character for \code{p.adjust} function from the
+#' \code{stats} package. Possible values are \code{"holm"}, \code{"hochberg"},
+#' \code{"hommel"}, \code{"bonferroni"}, \code{"BH"}, \code{"BY"}, \code{"fdr"}, \code{"none"}.
+#'
+#' Missing values are allowed but discarded for item estimation. They must be coded as \code{NA}
+#' for both, \code{data} and \code{group} parameters.
+#'
+#' @return A list with the following arguments:
+#' \describe{
+#'   \item{\code{Sval}}{the values of likelihood ratio test statistics.}
+#'   \item{\code{pval}}{the p-values by likelihood ratio test.}
+#'   \item{\code{adj.pval}}{the adjusted p-values by likelihood ratio test using \code{p.adjust.method}.}
+#'   \item{\code{df}}{the degress of freedom of likelihood ratio test.}
+#'   \item{\code{par.m0}}{the estimates of null model.}
+#'   \item{\code{par.m1}}{the estimates of alternative model.}
+#'   \item{\code{cov.m0}}{the estimates of covariance structure of null model.}
+#'   \item{\code{cov.m1}}{the estimates of covariance structure of alternative model.}
+#'   }
+#'
+#' @author
+#' Adela Drabinova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' Faculty of Mathematics and Physics, Charles University \cr
+#' adela.drabinova@gmail.com \cr
+#'
+#' Patricia Martinkova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' martinkova@cs.cas.cz \cr
+#'
+#'
+#' @seealso \code{\link[stats]{p.adjust}}
+#'
+#' @examples
+#' \dontrun{
+#' # loading data based on GMAT
+#' data(GMATtest, GMATkey)
+#'
+#' Data  <- GMATtest[, colnames(GMATtest) != "group"]
+#' group <- GMATtest[, "group"]
+#' key <- GMATkey
+#'
+#' # Testing both DIF effects
+#' MLR(Data, group, key, type = "both")
+#'
+#' # Testing uniform DIF effects
+#' MLR(Data, group, key, type = "udif")
+#'
+#' # Testing non-uniform DIF effects
+#' MLR(Data, group, key, type = "nudif")
+#' }
+#' @keywords DDF
+#' @export
+#' @importFrom stats coef vcov p.adjust.methods anova
+#' @importFrom nnet multinom
+#' @importFrom CTT score
+
+
+MLR <- function(Data, group, key, type = "both", p.adjust.method = "none", alpha = 0.05){
+
+  x <- scale(unlist(score(Data, key)))
+  m <- ncol(Data)
+  n <- nrow(Data)
+
+  m0 <- lapply(1:m, function(i) switch(type,
+                                       "both" = multinom(relevel(as.factor(Data[, i]),
+                                                                 ref = paste(key[i])) ~ x * group,
+                                                         trace = F),
+                                       "nudif" = multinom(relevel(as.factor(Data[, i]),
+                                                                  ref = paste(key[i])) ~ x * group,
+                                                          trace = F),
+                                       "udif" = multinom(relevel(as.factor(Data[, i]),
+                                                                 ref = paste(key[i])) ~ x + group,
+                                                         trace = F)))
+  m1 <- lapply(1:m, function(i) switch(type,
+                                       "both" = multinom(relevel(as.factor(Data[, i]),
+                                                                 ref = paste(key[i])) ~ x,
+                                                         trace = F),
+                                       "nudif" = multinom(relevel(as.factor(Data[, i]),
+                                                                  ref = paste(key[i])) ~ x + group,
+                                                          trace = F),
+                                       "udif" = multinom(relevel(as.factor(Data[, i]),
+                                                                 ref = paste(key[i])) ~ x,
+                                                         trace = F)))
+
+  MLRtest <- lapply(1:m, function(i) anova(m0[[i]], m1[[i]]))
+  MLRstat <- sapply(1:m, function(i) c(MLRtest[[i]]$`LR stat.`[2], MLRtest[[i]]$`Pr(Chi)`[2]))
+  df <- lapply(1:m, function(i) MLRtest[[i]]$`Resid. df`)
+
+  adjusted.pval <- p.adjust(MLRstat[2, ], method = p.adjust.methods)
+
+  par.m1 <- lapply(m1, coef)
+  par.m0 <- lapply(m0, coef)
+
+  cov.m1 <- lapply(m1, vcov)
+  cov.m0 <- lapply(m0, vcov)
+
+  # se.m1 <- sqrt(t(sapply(cov.m1, diag)))
+  # se.m0 <- sqrt(t(sapply(cov.m0, diag)))
+
+  # rownames(par.m1) <- rownames(par.m0) <- rownames(se.m1) <- rownames(se.m0) <- paste("Item", 1:m, sep = "")
+  results <- list(Sval = MLRstat[1, ],
+                  pval = MLRstat[2, ], adjusted.pval = adjusted.pval,
+                  df = df,
+                  par.m0 = par.m0, cov.m0 = cov.m0,
+                  par.m1 = par.m1, cov.m1 = cov.m1)
+  return(results)
+}
+
+
+
--- R/startNLR.R
+++ R/startNLR.R
@@ -1,12 +1,33 @@
 #' Calculates starting values for \code{difNLR} function.
 #'
-#' @param data numeric: a matrix or data.frame of binary data.
-#' @param group numeric: binary vector of group membership. "0" for reference group, "1" for focal group.
-#' @param parameterization character: parameterization of regression coefficients. Possible options are \code{"IRT"} (Item response theory, default option) and \code{"LR"} (Logistic regression)
+#' @param Data numeric: a matrix or data.frame of binary data.
+#' @param group numeric: binary vector of group membership.
+#' "0" for reference group, "1" for focal group.
+#' @param model character: generalized logistic regression model
+#' to be fitted. See \strong{Details}.
+#' @param parameterization character: parameterization of regression
+#' coefficients. Possible options are \code{"IRT"} (Item response theory,
+#' default option) and \code{"LR"} (Logistic regression)
 #'
-#' @description Calculates starting values for \code{difNLR} function based on linear approximation.
+#' @description Calculates starting values for \code{difNLR} function based
+#' on linear approximation.
 #'
-#' @usage startNLR(data, group, parameterization = "IRT")
+#' @usage startNLR(Data, group, model, parameterization = "IRT")
+#'
+#' @details
+#' The options of \code{model} are as follows: \code{Rasch} for one-parameter logistic model with
+#' discrimination parameter fixed on value 1 for both groups, \code{1PL} for one-parameter logistic
+#' model with discrimination parameter fixed for both groups, \code{2PL} for logistic regression model,
+#' \code{3PLcg} for three-parameter logistic regression model with fixed guessing for both groups,
+#' \code{3PLdg} for three-parameter logistic regression model with fixed inattention for both groups,
+#' \code{3PLc} and \code{3PL} for three-parameter logistic regression model with guessing parameter,
+#' \code{3PLd} for three-parameter logistic regression model with inattention parameter,
+#' \code{4PLcgdg} for four-parameter logistic regression model with fixed guessing and inattention
+#' parameter for both groups,
+#' \code{4PLcg} for four-parameter logistic regression model with fixed guessing for both groups,
+#' \code{4PLdg} for four-parameter logistic regression model with fixed inattention for both groups, or
+#' \code{4PL} for four-parameter logistic regression model.
+
 #'
 #' @return
 #' A data.frame containing rows representing items and 5 columns representing parameters of \code{difNLR} model. First column represents discrimination (a), second difficulty (b), third guessing (c), fourth difference in discrimination between reference and focal group (aDif) and fifth difference in difficulty between reference and focal group (bDif).
@@ -32,15 +53,24 @@
 #' # loading data based on GMAT
 #' data(GMAT)
 #'
-#' data  <- GMAT[, colnames(GMAT) != "group"]
+#' Data  <- GMAT[, colnames(GMAT) != "group"]
 #' group <- GMAT[, "group"]
 #'
-#' # starting values
-#' startNLR(data, group)
+#' # starting values for 3PL model
+#' startNLR(Data, group, model = "3PL")
 #' }
 #' @export
-startNLR <- function(data, group, parameterization = "IRT"){
-
+startNLR <- function(Data, group, model, parameterization = "IRT"){
+  if (missing(model)) {
+    stop("'model' is missing",
+         call. = FALSE)
+  } else {
+    if (!(model %in% c("Rasch", "1PL", "2PL", "3PLcg", "3PLdg", "3PLc", "3PLd", "3PL", "4plcgdg",
+                       "4PLcg", "4PLdg", "4PL"))){
+      stop("Invalid value for 'model'",
+           call. = FALSE)
+    }
+  }
   startNLR_line <- function(DATA){
     stand_total_score <- c(scale(apply(DATA, 1, sum)))
 
@@ -53,34 +83,65 @@ startNLR <- function(data, group, parameterization = "IRT"){
     u1 <- y[, 1] - x[, 1]
     u2 <- y[, 2] - x[, 2]
 
-    c <- -(-u1 * y[, 2] + u2 * y[, 1]) / u1
+    q <- -(-u1 * y[, 2] + u2 * y[, 1]) / u1
 
-    t <- u2 / u1
+    k <- u2 / u1
 
-    results <- as.data.frame(cbind(t, c))
+    results <- as.data.frame(cbind(k, q))
     return(results)
   }
-  line <- startNLR_line(data)
-  g <- apply(cbind(0, line$t * (-4) + line$c), 1, max)
 
-  group <- as.factor(group)
-  levels(group) <- c("R", "F")
+  line <- startNLR_line(Data)
 
-  data_R <- data[group == "R", ] ### reference group
-  data_F <- data[group == "F", ] ### foal group
+  data_R <- Data[group == 0, ] ### reference group
+  data_F <- Data[group == 1, ] ### foal group
   line_R <- startNLR_line(data_R)
   line_F <- startNLR_line(data_F)
 
-  b_R <- ((1 + g)/2 - line_R$c)/line_R$t
-  b_F <- ((1 + g)/2 - line_F$c)/line_F$t
+  if (model %in% c("Rasch", "1PL", "2PL", "3PLdg", "3PLd")){
+    c_R <- c_F <- 0
+  } else {
+    if (grepl("cg", model)){
+      c_R <- c_F <- apply(cbind(0, line$k * (-4) + line$q), 1, max)
+    } else {
+      c_R <- apply(cbind(0, line_R$k * (-4) + line_R$q), 1, max)
+      c_F <- apply(cbind(0, line_F$k * (-4) + line_F$q), 1, max)
+    }
+  }
 
-  a_R <- 4 * line_R$t/(1 - g)
-  a_F <- 4 * line_F$t/(1 - g)
+
+  if (model %in% c("Rasch", "1PL", "2PL", "3PLcg", "3PLc")){
+    d_R <- d_F <- 1
+  } else {
+    if (grepl("dg", model)){
+      d_R <- d_F <- apply(cbind(1, line$k * 4 + line$q), 1, min)
+    } else {
+      c_R <- apply(cbind(1, line_R$k * 4 + line_R$q), 1, min)
+      c_F <- apply(cbind(1, line_F$k * 4 + line_F$q), 1, min)
+    }
+  }
+
+  if (model == "Rasch"){
+    a_R <- a_F <- 1
+  } else {
+    if (model == "1PL") {
+      a_R <- a_F <- 4 * line$k/(1 - c_R)
+    } else {
+      a_R <- 4 * line_R$k/(1 - c_R)
+      a_F <- 4 * line_F$k/(1 - c_F)
+    }
+  }
+
+  b_R <- ((1 + c_R)/2 - line_R$q)/line_R$k
+  b_F <- ((1 + c_F)/2 - line_F$q)/line_F$k
+
 
   results <- switch(parameterization,
-                    IRT = data.frame(a_R, b_R, g, a_R - a_F, b_R - b_F),
-                    logistic = data.frame(a_R, -a_R * b_R, g, a_R - a_F, -a_R * b_R + a_F * b_F))
-  colnames(results) <- c("a", "b", "c", "aDif", "bDif")
+                    IRT = data.frame(a_R, b_R, c_R, d_R,
+                                     a_F - a_R, b_F - b_R, c_F - c_R, d_F - d_R),
+                    logistic = data.frame(a_R, -a_R * b_R, c_R, d_R,
+                                          a_F - a_R, -a_R * b_R + a_F * b_F, c_F - c_R, d_F - d_R))
+  colnames(results) <- c("a", "b", "c", "d", "aDif", "bDif", "cDif", "dDif")
   return(results)
 }
 
