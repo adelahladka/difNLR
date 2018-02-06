abb3197c2b746ca1d3ae918e353db0acc096d6a4
Adéla Drabinová <adela.drabinova@gmail.com>
2018-02-06 11:38:24 +0100

    Version 1.1.3
    See NEWS file.

---
+++ R/checkInterval.R
@@ -0,0 +1,34 @@
+#' Checks interval bounds.
+#'
+#' @param x numeric.
+#' @param vec numeric: increasingly sorted bounds of interval.
+#'
+#' @description Checks whether \code{x} lies in interval defined by bounds in \code{vec}. If it does,
+#' it returns value of \code{x}. In case that value of \code{x} is lower than lower bound specified
+#' in \code{vec}, it returns it value. In case that value of \code{x} is greater than upper bound specified
+#' in \code{vec}, it returns it value.
+#'
+#' @usage checkInterval(x, vec)
+#'
+#' @author
+#' Adela Drabinova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' Faculty of Mathematics and Physics, Charles University \cr
+#' drabinova@cs.cas.cz \cr
+#'
+#' Patricia Martinkova \cr
+#' Institute of Computer Science, The Czech Academy of Sciences \cr
+#' martinkova@cs.cas.cz \cr
+#'
+#' @examples
+#' \dontrun{
+#' checkInterval(x = 0.5, vec = c(0, 1))
+#' checkInterval(x = 1.5, vec = c(0, 1))
+#' checkInterval(x = -0.5, vec = c(0, 1))
+#' }
+#' @export
+
+checkInterval <- function(x, vec){
+  ifelse(x >= vec[1] & x <= vec[2], x,
+         ifelse(x > vec[2], vec[2], vec[1]))
+}
