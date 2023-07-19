e13c9a6dc5b302a33d19c0e91037bbf22b29fbbe
adelahladka <hladkadela@gmail.com>
2023-07-19 14:49:41 +0200

    Update formulaNLR.R
    formula update to implement EM and PLF

--- R/formulaNLR.R
+++ R/formulaNLR.R
@@ -54,7 +54,7 @@
 #' \code{"b"} between two groups and parameters \code{"c"} and \code{"d"} for focal group.
 #' \code{"logistic"} returns parameters in logistic regression parameterization and it is available only for 2PL model.
 #'
-#' @return A list of two models. Both includes formula, parameters to be estimated and their lower and upper constraints.
+#' @return A list of two models. Each includes a formula, parameters to be estimated, and their lower and upper constraints.
 #'
 #' @author
 #' Adela Hladka (nee Drabinova) \cr
@@ -101,12 +101,12 @@ formulaNLR <- function(model, constraints = NULL, type = "all", parameterization
       "3PLcg", "3PLdg", "3PLc", "3PL", "3PLd",
       "4PLcgdg", "4PLcgd", "4PLd", "4PLcdg", "4PLc", "4PL"
     ))) {
-      stop("Invalid value for 'model'.", call. = FALSE)
+      stop("Invalid value for the 'model' argument.", call. = FALSE)
     }
   }
 
   if (parameterization == "logistic" & model != "2PL") {
-    stop("Logistic parameterization is available only for 2PL model", call. = FALSE)
+    stop("Logistic parameterization is available only for the 2PL model", call. = FALSE)
   }
 
   # constraints for model
@@ -117,7 +117,7 @@ formulaNLR <- function(model, constraints = NULL, type = "all", parameterization
     if (any(!is.na(constraints))) {
       constr <- unlist(strsplit(constraints, split = ""))
       if (!all(constr %in% letters[1:4])) {
-        warning("Constraints can be only 'a', 'b', 'c' or 'd'!", call. = FALSE)
+        warning("Constraints can be only 'a', 'b', 'c', or 'd'!", call. = FALSE)
       }
       cons[paste0(constr, "Dif")] <- FALSE
     }
@@ -156,7 +156,7 @@ formulaNLR <- function(model, constraints = NULL, type = "all", parameterization
     types <- unlist(strsplit(type, split = ""))
     if (!all(types %in% letters[1:4])) {
       stop("Type of DIF to be tested not recognized. Only parameters 'a', 'b', 'c', or 'd' can be tested
-           or 'type' must be one of predefined options: either 'udif', 'nudif', 'both', or 'all'", call. = FALSE)
+           or 'type' must be one of predefined options: either 'udif', 'nudif', 'both', or 'all'.", call. = FALSE)
     }
     typcons[paste0(types, "Dif")] <- FALSE
     type <- "other"
@@ -205,105 +205,210 @@ formulaNLR <- function(model, constraints = NULL, type = "all", parameterization
   mod0 <- typ
   mod1 <- mod
 
-  if (parameterization == "logistic") {
+  if (parameterization == "XXX") {
     mod0 <- c(
-      as.logical(mod0[["b"]]),
-      as.logical(mod0[["a"]]),
-      as.logical(-mod0[["a"]] * mod0[["bDif"]] -
-        mod0[["aDif"]] * mod0[["b"]] -
-        mod0[["aDif"]] * mod0[["bDif"]]),
-      as.logical(mod0[["aDif"]])
+      "b0" = as.logical(mod0[["b"]]), # note, intercept always estimated, even if discrimination is not
+      "b1" = as.logical(mod0[["a"]]),
+      "b2" = as.logical(-mod0[["a"]] * mod0[["bDif"]] -
+                   mod0[["aDif"]] * mod0[["b"]] -
+                   mod0[["aDif"]] * mod0[["bDif"]]),
+      "b3" = as.logical(mod0[["aDif"]]),
+      "c" = as.logical(mod0[["c"]]),
+      "cDif" = as.logical(mod0[["cDif"]]),
+      "d" = as.logical(mod0[["d"]]),
+      "dDif" = as.logical(mod0[["dDif"]])
     )
     mod1 <- c(
-      as.logical(mod1[["b"]]),
-      as.logical(mod1[["a"]]),
-      as.logical(-mod1[["a"]] * mod1[["bDif"]] -
+      "b0" = as.logical(mod1[["b"]]), # note, intercept always estimated, even if discrimination is not
+      "b1" = as.logical(mod1[["a"]]),
+      "b2" = as.logical(-mod1[["a"]] * mod1[["bDif"]] -
+                   mod1[["aDif"]] * mod1[["b"]] -
+                   mod1[["aDif"]] * mod1[["bDif"]]),
+      "b3" = as.logical(mod1[["aDif"]]),
+      "c" = as.logical(mod1[["c"]]),
+      "cDif" = as.logical(mod1[["cDif"]]),
+      "d" = as.logical(mod1[["d"]]),
+      "dDif" = as.logical(mod1[["dDif"]])
+    )
+    # expit part
+    mod0_expit <- mod0[c("b0", "b1", "b2", "b3")]
+    part_expit0 <- paste0(paste0(names(mod0_expit), c("", " * x", " * g", " * x * g"))[mod0_expit], collapse = " + ")
+    part_expit0 <- paste0(" / (1 + exp(-(", part_expit0, ")))")
+    # c asymptote
+    mod0_c0 <- mod0[c("c", "cDif")]
+    part_c0 <- paste0(paste0(names(mod0_c0), c("", " * g"))[mod0_c0], collapse = " + ")
+    # d asymptote
+    mod0_d0 <- mod0[c("d", "dDif")]
+    part_d0 <- paste0(paste0(names(mod0_d0), c("", " * g"))[mod0_d0], collapse = " + ")
+    part_d0 <- ifelse(part_d0 == "", 1, part_d0)
+    # d - c
+    part_dc0 <- paste0("(", part_d0, " - ", ifelse(nchar(part_c0) == 1, part_c0, paste0("(", part_c0, ")")), ")")
+    part_dc0 <- ifelse(part_dc0 == "", 1, part_dc0)
+    # formula
+    form0 <- paste0(paste(c(part_c0, part_dc0), collapse = " + "), part_expit0, collapse = " * ")
+
+    # expit part
+    mod1_expit <- mod1[c("b0", "b1", "b2", "b3")]
+    part_expit1 <- paste0(paste0(names(mod1_expit), c("", " * x", " * g", " * x * g"))[mod1_expit], collapse = " + ")
+    part_expit1 <- paste0(" / (1 + exp(-(", part_expit1, ")))")
+    # c asymptote
+    mod1_c1 <- mod1[c("c", "cDif")]
+    part_c1 <- paste0(paste0(names(mod1_c1), c("", " * g"))[mod1_c1], collapse = " + ")
+    # d asymptote
+    mod1_d1 <- mod1[c("d", "dDif")]
+    part_d1 <- paste0(paste0(names(mod1_d1), c("", " * g"))[mod1_d1], collapse = " + ")
+    part_d1 <- ifelse(part_d1 == "", 1, part_d1)
+    # d - c
+    part_dc1 <- paste0("(", part_d1, " - ", ifelse(nchar(part_c1) == 1, part_c1, paste0("(", part_c1, ")")), ")")
+    part_dc1 <- ifelse(part_dc1 == "", 1, part_dc1)
+    # formula
+    form1 <- paste0(paste(c(part_c1, part_dc1), collapse = " + "), part_expit1, collapse = " * ")
+
+  } else if (parameterization == "logistic") {
+    mod0 <- c(
+      "(Intercept)" = as.logical(mod0[["b"]]), # note, intercept always estimated, even if discrimination is not
+      "x" = as.logical(mod0[["a"]]),
+      "g" = as.logical(-mod0[["a"]] * mod0[["bDif"]] -
+        mod0[["aDif"]] * mod0[["b"]] -
+        mod0[["aDif"]] * mod0[["bDif"]]),
+      "x:g" = as.logical(mod0[["aDif"]])
+    )
+    mod1 <- c(
+      "(Intercept)" = as.logical(mod1[["b"]]), # note, intercept always estimated, even if discrimination is not
+      "x" = as.logical(mod1[["a"]]),
+      "g" = as.logical(-mod1[["a"]] * mod1[["bDif"]] -
         mod1[["aDif"]] * mod1[["b"]] -
         mod1[["aDif"]] * mod1[["bDif"]]),
-      as.logical(mod1[["aDif"]])
+      "x:g" = as.logical(mod1[["aDif"]])
     )
-    names(mod0) <- names(mod1) <- c("(Intercept)", "x", "g", "x:g")
     part_regr0 <- names(mod0[-1])[mod0[-1]]
-    part_regr0 <- paste(part_regr0[part_regr0 != ""], collapse = " + ")
+    form0 <- paste(part_regr0[part_regr0 != ""], collapse = " + ")
     part_regr1 <- names(mod1[-1])[mod1[-1]]
-    part_regr1 <- paste(part_regr1[part_regr1 != ""], collapse = " + ")
+    form1 <- paste(part_regr1[part_regr1 != ""], collapse = " + ")
   } else {
-    # parameters in model 0 and model 1
-    param0 <- param1 <- list()
 
-    for (i in 1:4) {
-      param0[[i]] <- paste(names(which(mod0[c(i, i + 4)])), collapse = " + g * ")
-      param1[[i]] <- paste(names(which(mod1[c(i, i + 4)])), collapse = " + g * ")
-    }
+    # expit part - a
+    mod0_expit_a0 <- mod0[c("a", "aDif")]
+    part_a0 <- paste0(paste0(names(mod0_expit_a0), c("", " * g"))[mod0_expit_a0], collapse = " + ")
+    part_a0 <- ifelse(nchar(part_a0) == 1, part_a0, paste0("(", part_a0, ")"))
+    # expit part - b
+    mod0_expit_b0 <- mod0[c("b", "bDif")]
+    part_b0 <- paste0(paste0(names(mod0_expit_b0), c("", " * g"))[mod0_expit_b0], collapse = " + ")
+    part_b0 <- ifelse(nchar(part_b0) == 1, part_b0, paste0("(", part_b0, ")"))
+    # expit part
+    part_expit0 <- paste0("/ (1 + exp(-", paste(c(part_a0, paste0("(x - ", part_b0, ")")), collapse = " * "), ")")
+    # c asymptote
+    mod0_c0 <- mod0[c("c", "cDif")]
+    part_c0 <- paste0(paste0(names(mod0_c0), c("", " * g"))[mod0_c0], collapse = " + ")
+    # d asymptote
+    mod0_d0 <- mod0[c("d", "dDif")]
+    part_d0 <- paste0(paste0(names(mod0_d0), c("", " * g"))[mod0_d0], collapse = " + ")
+    part_d0 <- ifelse(part_d0 == "", 1, part_d0)
+    # d - c
+    part_dc0 <- paste0("(", part_d0, " - ", ifelse(nchar(part_c0) == 1, part_c0, paste0("(", part_c0, ")")), ")")
+    part_dc0 <- ifelse(part_dc0 == "", 1, part_dc0)
+    # formula
+    form0 <- paste0(paste(c(part_c0, part_dc0), collapse = " + "), part_expit0, collapse = " * ")
 
-    if (parameterization == "alternative") {
-      names(mod0)[3:4] <- names(mod1)[3:4] <- c("cR", "dR")
-      names(mod0)[7:8] <- names(mod1)[7:8] <- c("cF", "dF")
-      for (i in 3:4) {
-        param0[[i]] <- ifelse(length(which(mod0[c(i, i + 4)])) == 1,
-          names(which(mod0[c(i, i + 4)])),
-          paste(names(which(mod0[c(i, i + 4)])), c("* (1 - g)", "* g")[mod0[c(i, i + 4)]], collapse = " + ")
-        )
-        param1[[i]] <- ifelse(length(which(mod1[c(i, i + 4)])) == 1,
-          names(which(mod1[c(i, i + 4)])),
-          paste(names(which(mod1[c(i, i + 4)])), c("* (1 - g)", "* g")[mod1[c(i, i + 4)]], collapse = " + ")
-        )
-      }
-    }
-
-    if (param0[[3]] == "") param0[[3]] <- "0"
-    if (param0[[4]] == "") param0[[4]] <- "1"
-
-    if (param1[[3]] == "") param1[[3]] <- "0"
-    if (param1[[4]] == "") param1[[4]] <- "1"
-
-    if (param0[[1]] == "") param0[[1]] <- "1"
-    if (param1[[1]] == "") param1[[1]] <- "1"
-
-    # formulas for model 0 and model 1
-    param0[grepl(" ", param0)] <- paste0("(", param0[grepl(" ", param0)], ")")
-    param1[grepl(" ", param1)] <- paste0("(", param1[grepl(" ", param1)], ")")
-
-    # (d - c)
-    part_dc0 <- paste(c(param0[4], param0[3]), collapse = " - ")
-    part_dc0[grepl(" ", part_dc0)] <- paste0("(", part_dc0[grepl(" ", part_dc0)], ")")
-    part_dc1 <- paste(c(param1[4], param1[3]), collapse = " - ")
-    part_dc1[grepl(" ", part_dc1)] <- paste0("(", part_dc1[grepl(" ", part_dc1)], ")")
-
-    # c + (d - c)
-    part_cdc0 <- paste(c(param0[3], part_dc0), collapse = " + ")
-    part_cdc1 <- paste(c(param1[3], part_dc1), collapse = " + ")
-
-    # x - b
-    part_xb0 <- paste0("(x - ", param0[2], ")")
-    part_xb1 <- paste0("(x - ", param1[2], ")")
-
-    # a * (x - b)
-    part_axb0 <- paste(param0[1], " * ", part_xb0)
-    part_axb1 <- paste(param1[1], " * ", part_xb1)
-
-    # 1 + exp(- a * (x - b))
-    if (parameterization == "logistic") {
-      part_exp0 <- part_axb0
-      part_exp1 <- part_axb1
-    } else {
-      part_exp0 <- paste0("1 + exp(-", part_axb0, ")")
-      part_exp0 <- paste0("(", part_exp0, ")")
-      part_exp1 <- paste0("1 + exp(-", part_axb1, ")")
-      part_exp1 <- paste0("(", part_exp1, ")")
-    }
-
-    # model
-    part_regr0 <- paste(part_cdc0, "/", part_exp0)
-    part_regr1 <- paste(part_cdc1, "/", part_exp1)
+    # expit part - a
+    mod1_expit_a1 <- mod1[c("a", "aDif")]
+    part_a1 <- paste0(paste0(names(mod1_expit_a1), c("", " * g"))[mod1_expit_a1], collapse = " + ")
+    part_a1 <- ifelse(nchar(part_a1) == 1, part_a1, paste0("(", part_a1, ")"))
+    # expit part - b
+    mod1_expit_b1 <- mod1[c("b", "bDif")]
+    part_b1 <- paste0(paste0(names(mod1_expit_b1), c("", " * g"))[mod1_expit_b1], collapse = " + ")
+    part_b1 <- ifelse(nchar(part_b1) == 1, part_b1, paste0("(", part_b1, ")"))
+    # expit part
+    part_expit1 <- paste0("/ (1 + exp(-", paste(c(part_a1, paste0("(x - ", part_b1, ")")), collapse = " * "), ")")
+    # c asymptote
+    mod1_c1 <- mod1[c("c", "cDif")]
+    part_c1 <- paste0(paste0(names(mod1_c1), c("", " * g"))[mod1_c1], collapse = " + ")
+    # d asymptote
+    mod1_d1 <- mod1[c("d", "dDif")]
+    part_d1 <- paste0(paste0(names(mod1_d1), c("", " * g"))[mod1_d1], collapse = " + ")
+    part_d1 <- ifelse(part_d1 == "", 1, part_d1)
+    # d - c
+    part_dc1 <- paste0("(", part_d1, " - ", ifelse(nchar(part_c1) == 1, part_c1, paste0("(", part_c1, ")")), ")")
+    part_dc1 <- ifelse(part_dc1 == "", 1, part_dc1)
+    # formula
+    form1 <- paste0(paste(c(part_c1, part_dc1), collapse = " + "), part_expit1, collapse = " * ")
   }
+  #   # parameters in model 0 and model 1
+  #   param0 <- param1 <- list()
+  #
+  #   for (i in 1:4) {
+  #     param0[[i]] <- paste(names(which(mod0[c(i, i + 4)])), collapse = " + g * ")
+  #     param1[[i]] <- paste(names(which(mod1[c(i, i + 4)])), collapse = " + g * ")
+  #   }
+  #
+  #   if (parameterization == "alternative") {
+  #     names(mod0)[3:4] <- names(mod1)[3:4] <- c("cR", "dR")
+  #     names(mod0)[7:8] <- names(mod1)[7:8] <- c("cF", "dF")
+  #     for (i in 3:4) {
+  #       param0[[i]] <- ifelse(length(which(mod0[c(i, i + 4)])) == 1,
+  #         names(which(mod0[c(i, i + 4)])),
+  #         paste(names(which(mod0[c(i, i + 4)])), c("* (1 - g)", "* g")[mod0[c(i, i + 4)]], collapse = " + ")
+  #       )
+  #       param1[[i]] <- ifelse(length(which(mod1[c(i, i + 4)])) == 1,
+  #         names(which(mod1[c(i, i + 4)])),
+  #         paste(names(which(mod1[c(i, i + 4)])), c("* (1 - g)", "* g")[mod1[c(i, i + 4)]], collapse = " + ")
+  #       )
+  #     }
+  #   }
+  #
+  #   if (param0[[3]] == "") param0[[3]] <- "0"
+  #   if (param0[[4]] == "") param0[[4]] <- "1"
+  #
+  #   if (param1[[3]] == "") param1[[3]] <- "0"
+  #   if (param1[[4]] == "") param1[[4]] <- "1"
+  #
+  #   if (param0[[1]] == "") param0[[1]] <- "1"
+  #   if (param1[[1]] == "") param1[[1]] <- "1"
+  #
+  #   # formulas for model 0 and model 1
+  #   param0[grepl(" ", param0)] <- paste0("(", param0[grepl(" ", param0)], ")")
+  #   param1[grepl(" ", param1)] <- paste0("(", param1[grepl(" ", param1)], ")")
+  #
+  #   # (d - c)
+  #   part_dc0 <- paste(c(param0[4], param0[3]), collapse = " - ")
+  #   part_dc0[grepl(" ", part_dc0)] <- paste0("(", part_dc0[grepl(" ", part_dc0)], ")")
+  #   part_dc1 <- paste(c(param1[4], param1[3]), collapse = " - ")
+  #   part_dc1[grepl(" ", part_dc1)] <- paste0("(", part_dc1[grepl(" ", part_dc1)], ")")
+  #
+  #   # c + (d - c)
+  #   part_cdc0 <- paste(c(param0[3], part_dc0), collapse = " + ")
+  #   part_cdc1 <- paste(c(param1[3], part_dc1), collapse = " + ")
+  #
+  #   # x - b
+  #   part_xb0 <- paste0("(x - ", param0[2], ")")
+  #   part_xb1 <- paste0("(x - ", param1[2], ")")
+  #
+  #   # a * (x - b)
+  #   part_axb0 <- paste(param0[1], " * ", part_xb0)
+  #   part_axb1 <- paste(param1[1], " * ", part_xb1)
+  #
+  #   # 1 + exp(- a * (x - b))
+  #   if (parameterization == "logistic") {
+  #     part_exp0 <- part_axb0
+  #     part_exp1 <- part_axb1
+  #   } else {
+  #     part_exp0 <- paste0("1 + exp(-", part_axb0, ")")
+  #     part_exp0 <- paste0("(", part_exp0, ")")
+  #     part_exp1 <- paste0("1 + exp(-", part_axb1, ")")
+  #     part_exp1 <- paste0("(", part_exp1, ")")
+  #   }
+  #
+  #   # model
+  #   part_regr0 <- paste(part_cdc0, "/", part_exp0)
+  #   part_regr1 <- paste(part_cdc1, "/", part_exp1)
+  # }
 
   # formula - adding outcome
   if (missing(outcome)) {
     outcome <- "y"
   }
-  form0 <- paste(outcome, "~", part_regr0)
-  form1 <- paste(outcome, "~", part_regr1)
+  form0 <- paste(outcome, "~", form0)
+  form1 <- paste(outcome, "~", form1)
 
   lower <- switch(parameterization,
     "classic" = c(-Inf, -Inf, 0, 0, -Inf, -Inf, -1, -1),
